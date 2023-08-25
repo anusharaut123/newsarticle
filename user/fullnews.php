b697ce3d02b946b28d6060ebc7bf9fcb6c883c65
mostanglan <xsonxhrestha2016@gmail.com>
2023-08-25 22:28:00 +0545

    updates

--- user/fullnews.php
+++ user/fullnews.php
@@ -1,34 +1,27 @@
 
 <?php
 session_start();
+$newsid = $_GET['newsid'];
 require "../db/connection.php";
   if($_SERVER["REQUEST_METHOD"]=="POST"){
     if(isset($_POST['submit'])){
         $userid = $_SESSION['userid'];
-        $newsid = $_GET['newsid'];
-        $name=$_POST['name'];
+        $username=$_SESSION['username'];
         $comment=$_POST['comment'];
-        $sql = "INSERT INTO comments (userid, newsid, name, comment) VALUES ('$name', '$comment')";
+        unset($_POST);
+        $sql = "INSERT INTO comments (userid, newsid, username, comment) VALUES ('$userid', '$newsid', '$username', '$comment')";
         if(mysqli_query($conn, $sql)){ 
           echo "Comment added successfully.";
         } else {
           echo "Error";
         }
       }
-      $sql = "SELECT name, comment FROM comments";
-      $result=mysqli_query($conn, $query);
-      $data= mysqli_num_rows($result);
-      if($data>0){
-          while($row=mysqli_fetch_array($result)){
-            echo '<div class="comment">';
-            echo '<h4>' . $row['name'] . '</h4>';
-            echo '<p>' . $row['comment'] . '</p>';
-            echo '</div>';
-        }
-    }
   }
+$query = "SELECT * FROM news WHERE newsid='$newsid' LIMIt 1";
+$newsresult = mysqli_query($conn, $query);
+$newsdata = mysqli_fetch_array($newsresult);
 
-    ?>
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -102,45 +95,45 @@ require "../db/connection.php";
 </head>
 <body>
   <div class="article">
-    <h2 class="title">News Article Title</h2>
-    <p class="author">Author Name</p>
+    <h2 class="title"><?php echo $newsdata['title']; ?></h2>
+    <p class="author"><?php echo $newsdata['authorname']; ?></p>
     <p class="date">Saturday, July 8, 2023 </p>
-    <img style="width: 100%; height: 500px" class="image" src="../images/imagess.jpg" alt="News Image">
+    <?php
+    $imageid = $newsdata['newsid'];
+    $imagequery = "SELECT * FROM news_image WHERE newsid = '$imageid' LIMIT 3";
+    $imageresult = mysqli_query($conn, $imagequery);
+    $imagerow = mysqli_fetch_array($imageresult);
+    ?>
+    <img style="width: 100%; height: 500px" class="image" src="../admin/newsimage/<?php echo $imagerow['imagename'] ?>" alt="News Image">
     <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
-      <p>Donec venenatis est eu dolor feugiat, in commodo lectus malesuada. Curabitur in consectetur sem. Integer facilisis, ipsum nec dapibus ullamcorper, erat justo tincidunt metus, in lacinia mauris odio vel nisl. Quisque sollicitudin, nisl vel facilisis convallis, urna elit feugiat magna, in suscipit nunc sem eu orci.</p>
+      <p><?php echo $newsdata['introduction']; ?></p>
     </div>
-    <img style="height: 350px; width: 725px;" class="image-left" src="../images/download.jpg" alt="Left Image">
-    <img style="height: 350px; width: 725px;" class="image-right" src="../images/background-image.jpg" alt="Right Image">
+    <?php $imagerow = mysqli_fetch_array($imageresult); ?>
+    <img style="height: 350px; width: 725px;" class="image-left" src="../admin/newsimage/<?php echo $imagerow['imagename'] ?>" alt="Left Image">
+    <?php $imagerow = mysqli_fetch_array($imageresult); ?>
+    <img style="height: 350px; width: 725px;" class="image-right" src="../admin/newsimage/<?php echo $imagerow['imagename'] ?>" alt="Right Image">
     <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et volutpat felis. Nulla pulvinar auctor erat, vel fringilla ligula ultrices nec. Proin vehicula fringilla elit, vitae pellentesque mi sollicitudin non. Sed tempus, felis sit amet accumsan faucibus, risus urna imperdiet nunc, a tristique mauris lectus at mauris.</p>
+      <p><?php echo $newsdata['description']; ?></p>
     </div>
     <div class="form-group">
-      <label for="comment">Comment:</label>
-      <textarea class="form-control" rows="7" cols="50" name="comment" id="comment"></textarea><br>
-      <input type="submit" name="comment" class="btn btn-primary" value="Comment">
+      <form action="fullnews.php?newsid=<?php echo $newsid ?>" method="post">
+        <label for="comment">Comment:</label>
+        <textarea class="form-control" rows="7" cols="50" name="comment" id="comment"></textarea><br>
+        <input type="submit" name="submit" class="btn btn-primary" value="Comment">
+      </form>
     </div>
-
-    <div class="comment">
-        <h4>Anusha Raut</h4>
-        <p>This is a great article. I really enjoyed reading it!</p>
-    </div>
-    
-    <div class="comment">
-        <h4>Sanish Shrestha</h4>
-        <p>Thanks for sharing this information. It's very helpful.</p>
-    </div>
-        <form>
-        <h4 style="color: blue">Add your comment:</h4>
-        <h4>User Name: <?php echo $_SESSION['username']; ?></h4>
-        <textarea id="comment" name="comment" placeholder="Your comment" required></textarea><br>
-        <button type="submit" id="submit" name="submit">Submit</button>
-    </form>
-
-  </div>
+    <?php
+      $sql = "SELECT username, comment FROM comments";
+      $result=mysqli_query($conn, $sql);
+      $data= mysqli_num_rows($result);
+      if($data>0){
+          while($row=mysqli_fetch_array($result)){
+            echo '<div class="comment">';
+            echo '<h4>' . $row['username'] . '</h4>';
+            echo '<p>' . $row['comment'] . '</p>';
+            echo '</div>';
+        }
+    }
+    ?>
 </body>
 </html>
